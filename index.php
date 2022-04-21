2e0ba37b13ec790e4d43e29529f7153ff884e8da
anniecmb <annieclaude.mb@gmail.com>
2022-04-21 13:44:21 -0400

    Ajout HTML

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
 
 $cartes=["As",2,3,4,5,6,7,8,9,10,"Valet","Dame","Roi"];
 $suites=["♦", "♣", "♥", "♠"];
@@ -35,3 +44,5 @@ for ($i=0; $i<count($suffledDeck); $i++){
 }
 
 ?>
+</body>
+</html>
